8cb23868dcc575ebae910743553c618bb1a3b75e
Renan Curvo <renan.curvo@gmail.com>
2022-03-16 19:01:42 -0300

    new version without draw

--- sketch.js
+++ sketch.js
@@ -3,36 +3,34 @@ const lenghtDensity = density.length
 let photo;
 
 function preload(){
-  photo = loadImage('pippa2lowres.jpg')
+  photo = loadImage('pippa2.png')
 }
 
 
 function setup() {
-  createCanvas(640, 1136);
-}
+noCanvas()
 
-function draw() {
   background(0);
 
 
-  let w = width/photo.width;
-  let h = height/photo.height;
   photo.loadPixels();
-  for (let i = 0; i < photo.width; i++){
-    for (let j = 0; j < photo.height; j++){
+  for (let j = 0; j < photo.height; j++){
+    let row ='';
+    for (let i = 0; i < photo.width; i++){
       const pixelIndex = (i + j*photo.width)*4;
       const r = photo.pixels[pixelIndex + 0];
       const g = photo.pixels[pixelIndex + 1];
       const b = photo.pixels[pixelIndex + 2];
       const averageBrightness = (r + g + b)/ 3;
-      noStroke();
-      fill(255);
-      // square(i*w, j*h,w);
       const charIndex = floor(map(averageBrightness, 255, 0, 0, lenghtDensity));
 
-      textSize(w);
-      textAlign(CENTER, CENTER);
-      text(density.charAt(charIndex), i*w + w*0.5, j*h + h*0.5);
+      const html = density.charAt(charIndex);
+      if (html == '') row += '&nbsp;'
+      else row += html;
+
+
+
     }
+    createDiv(row);
   }
 }
